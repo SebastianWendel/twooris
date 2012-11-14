1d3c44db0aa7a25b19fe8d26b56ae04f64b842b4
sebwendel <s.wendel@sourceindex.de>
2012-11-14 01:51:41 +0100

    first commit

--- twooris.py
+++ twooris.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python
+
+__author__      = "sebastiian wendel"
+__copyright__   = "copyright 2012, attraktor e.v."
+__license__     = "glp"
+__version__     = "0.0.1"
+__status__      = "production"
+
 import urllib2
+
 response = urllib2.urlopen('http://dooris.koalo.de/door.txt')
 html = response.read()
+
+
 print html
